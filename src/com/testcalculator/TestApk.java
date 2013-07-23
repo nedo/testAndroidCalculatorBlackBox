f4a1a4feed82e2a034f9776ea0c14da289ecd170
naveed <luckynaveed@gmail.com>
2013-07-23 14:14:46 +0500

    1. Added Robotium Latest 4.2 Jar 2. Some improvements

--- src/com/testcalculator/TestApk.java
+++ src/com/testcalculator/TestApk.java
@@ -2,54 +2,52 @@ package com.testcalculator;
 
 import com.jayway.android.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
 @SuppressWarnings("unchecked")
-public class TestApk extends ActivityInstrumentationTestCase2{
+public class TestApk extends ActivityInstrumentationTestCase2 {
 
-private static final String TARGET_PACKAGE_ID="com.calculator";
-private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME="com.calculator.Main";
-private static Class launcherActivityClass;
-static{
+	private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.calculator.Main";
+	private static Class launcherActivityClass;
+	static {
 
-try
-{
-launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
-} catch (ClassNotFoundException e){
-throw new RuntimeException(e);
-}
-}
+		try {
+			launcherActivityClass = Class
+					.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-public TestApk()throws ClassNotFoundException{
-super(TARGET_PACKAGE_ID,launcherActivityClass);
-}
+	public TestApk() throws ClassNotFoundException {
+		super(launcherActivityClass);
+	}
 
-private Solo solo;
+	private Solo solo;
 
-@Override
-protected void setUp() throws Exception
-{
-solo = new Solo(getInstrumentation(),getActivity());
-}
+	@Override
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
 
-public void testDisplayBlackBox() {
-//Enter any integer/decimal value for first editfield, we are writing 10
-	
-solo.enterText(0, "10");
+	public void testDisplayBlackBox() {
+		// Enter any integer/decimal value for first editfield, we are writing  10
+		solo.clearEditText(0);
+		solo.enterText(0, "10");
+		
+		// Enter any integer/decimal value for first editfield, we are writing  20
+		solo.clearEditText(1);
+		solo.enterText(1, "20");
+		
+		// Click on Multiply button
+		solo.clickOnButton("Multiply");
 
-//Enter any interger/decimal value for first editfield, we are writing 20
-solo.enterText(1, "20");
+		// Verify that resultant of 10 x 20
+		assertTrue(solo.searchText("200"));
+	}
 
-//Click on Multiply button
-solo.clickOnButton("Multiply");
-
-//Verify that resultant of 10 x 20
-assertTrue(solo.searchText("200"));
-}
-
-@Override
-public void tearDown() throws Exception {
-solo.finishOpenedActivities();
-}
+	@Override
+	public void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
 
 }
